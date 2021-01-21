c4e69fd9e1e152a7270e8e3354cdc9a441657b4b
MichelPodkhvatiln <podkhvatilin.work@gmail.com>
2021-01-21 22:18:11 +0200

    add find by id test

---
+++ test/finding_test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+describe('Finding records', () => {
+  let char;
+
+  beforeEach((done) => {
+    const charParams = {
+      name: 'Mario',
+    };
+    char = new MarioChar(charParams);
+
+    char.save().then(() => {
+      assert(char.isNew === false);
+      done();
+    });
+  });
+
+  it('Find record test', (done) => {
+    MarioChar.findOne({name: 'Mario'}).then((result) => {
+      assert(result.name === 'Mario');
+      done();
+    });
+  });
+
+  it('Find record by ID test', (done) => {
+    if (!char) {
+      return
+    }
+
+    MarioChar.findOne({_id: char.id}).then((result) => {
+      assert(result._id.toString() === char._id.toString());
+      done();
+    });
+  });
+});
